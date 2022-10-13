f6fd66cf806aa714d56616459dab219a60c9e43d
Анна <95694469+An-Kanata@users.noreply.github.com>
2022-10-13 23:01:20 +0300

    Update db.php

--- article/db.php
+++ article/db.php
@@ -1,3 +1,3 @@
 <?php
-$connection_loc = mysqli_connect('82.146.52.18', 'anko163', 'password', 'test_db');
+$connection_loc = mysqli_connect('82.146.52.18', 'test', 'test', 'test_db');
 mysqli_set_charset($connection_loc, 'utf8');
